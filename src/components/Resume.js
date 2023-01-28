ec798f0e5ac2d9dff22eaf7aca623e9b5284a6c0
Abhishree <abhishree881@gmail.com>
2023-01-28 12:12:57 +0530

    updated resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -15,7 +15,7 @@ export default function Resume({ status, message, onValidated }) {
             <div className="new-email-bx">
               <button type="submit">
                 <a
-                  href="https://drive.google.com/file/d/139uL_nJq1ATGbtk9a8-77Rrfbta81ZDM/view?usp=sharing "
+                  href="https://drive.google.com/file/d/1puFvcDjXMsm1a2erYMUsos5H_DXbkXeW/view?usp=sharing"
                   target="_blank"
                   download
                 >
