60caeb67c7cf6f0a9bf11393d6ab782c4c39fbc8
Abhishree881 <abhishree881@gmail.com>
2024-12-19 18:15:08 +0530

    changed resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -8,18 +8,18 @@ export default function Resume({ status, message, onValidated }) {
         <Row>
           <Col lg={12} md={6} xl={5}>
             <h3>
-              Click the button to <br></br>download resume
+              Click the button to <br></br>view resume
             </h3>
           </Col>
           <Col md={6} xl={7}>
             <div className="new-email-bx">
               <button>
                 <a
-                  href="https://drive.google.com/file/d/120v1ZFUGQPPSYLxXLZ0j3ewKWA62QuCY/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1QuQ7UHXHgdUjUp38LVeD_qGlDWK8Mrce/view?usp=sharing"
                   target="_blank"
                   download
                 >
-                  Download
+                  View Resume
                 </a>
               </button>
             </div>
